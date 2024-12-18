dfb9a766a6e7958a581ed12356ab0dcb315cacae
tharindi22 <ttharindiattanayaka@gmail.com>
2024-12-18 17:55:31 +0530

    create and organize script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("List-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert
+    }
+}
